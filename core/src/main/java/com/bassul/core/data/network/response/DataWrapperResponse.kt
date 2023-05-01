047c32ef986602a4f79faeff715fbb0ba774a356
mellina.cerqueira <mellinacerqueira@gmail.com>
2023-05-01 10:53:44 -0300

    Mapeamento dos objetos de response da api

---
+++ core/src/main/java/com/bassul/core/data/network/response/DataWrapperResponse.kt
@@ -0,0 +1,5 @@
+package com.bassul.core.data.network.response
+
+data class DataWrapperResponse(
+    val recipes: List<RecipeResponse>,
+)
---
+++ core/src/main/java/com/bassul/core/data/network/response/RecipeResponse.kt
@@ -0,0 +1,6 @@
+package com.bassul.core.data.network.response
+
+data class RecipeResponse(
+    val title: String,
+    val image: String
+)
