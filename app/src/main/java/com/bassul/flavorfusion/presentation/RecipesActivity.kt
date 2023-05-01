64fb2b13dc8f19f3f42481247608005597ac7cb1
mellina.cerqueira <mellinacerqueira@gmail.com>
2023-04-30 23:29:50 -0300

    Rodando deteck e criado RecipesActivity

---
+++ app/src/main/java/com/bassul/flavorfusion/presentation/RecipesActivity.kt
@@ -0,0 +1,15 @@
+package com.bassul.flavorfusion.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bassul.flavorfusion.R
+import com.bassul.flavorfusion.databinding.ActivityRecipesBinding
+
+class RecipesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipes)
+
+
+    }
+}
