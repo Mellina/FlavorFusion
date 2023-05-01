f08a2cf46dba4169e5e8649c1ee3b9eb4b17ec47
mellina.cerqueira <mellinacerqueira@gmail.com>
2023-04-30 23:36:43 -0300

    Configurado container de navegação

--- app/src/main/java/com/bassul/flavorfusion/presentation/RecipesActivity.kt
+++ app/src/main/java/com/bassul/flavorfusion/presentation/RecipesActivity.kt
@@ -2,14 +2,28 @@ package com.bassul.flavorfusion.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.bassul.flavorfusion.R
 import com.bassul.flavorfusion.databinding.ActivityRecipesBinding
 
 class RecipesActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recipes)
 
+        val binding = ActivityRecipesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_post_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
 
     }
 }
