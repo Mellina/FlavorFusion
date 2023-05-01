c2d7ba291a1c99cff852c1015b0e0954e931831a
mellina.cerqueira <mellinacerqueira@gmail.com>
2023-05-01 15:20:08 -0300

    Criado RecipesViewHolder

---
+++ app/src/main/java/com/bassul/flavorfusion/presentation/recipes/RecipesViewHolder.kt
@@ -0,0 +1,33 @@
+package com.bassul.flavorfusion.presentation.recipes
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bassul.core.domain.model.Recipe
+import com.bassul.flavorfusion.databinding.ItemRecipeBinding
+import com.bumptech.glide.Glide
+
+class RecipesViewHolder(
+    itemRecipesBinding: ItemRecipeBinding
+): ViewHolder(itemRecipesBinding.root) {
+
+    private val textTitle = itemRecipesBinding.textTitle
+    private val imageRecipe = itemRecipesBinding.imageRecipe
+
+    fun bind(recipe: Recipe) {
+        textTitle.text = recipe.title
+        Glide.with(itemView)
+            .load(recipe.image)
+            .into(imageRecipe)
+    }
+
+    companion object {
+        fun create(
+            parent: ViewGroup
+        ): RecipesViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+         val itemBinding = ItemRecipeBinding.inflate(inflater, parent, false)
+            return RecipesViewHolder(itemBinding)
+        }
+    }
+}
---
+++ core/src/main/java/com/bassul/core/domain/model/Recipe.kt
@@ -0,0 +1,6 @@
+package com.bassul.core.domain.model
+
+data class Recipe(
+    val title: String,
+    val image: String
+)
