16c2e26c1af59543c3cb3134d22c8368d435c5e4
mellina.cerqueira <mellinacerqueira@gmail.com>
2023-05-20 19:01:50 -0300

    Implementado teste unitário de de erro de RecipesPagingSource

--- app/src/test/java/com/bassul/flavorfusion/framework/paging/RecipesPagingSourceTest.kt
+++ app/src/test/java/com/bassul/flavorfusion/framework/paging/RecipesPagingSourceTest.kt
@@ -8,6 +8,7 @@ import com.bassul.testing.MainCoroutineRule
 import com.bassul.testing.model.RecipeFactory
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.*
 import org.junit.Before
@@ -37,6 +38,7 @@ class RecipesPagingSourceTest {
         recipesPagingSource = RecipesPagingSource(remoteDataSource, "")
     }
 
+    @ExperimentalCoroutinesApi
     @Test
     fun `should return a success load result w3hen load is called`() = runBlockingTest {
         // Arrange
@@ -64,6 +66,30 @@ class RecipesPagingSourceTest {
                 null,
                 2
             ),
+            result
+        )
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `should return a error load result when load is called`() = runBlockingTest {
+        //Arrange
+        val exception = RuntimeException()
+        whenever(remoteDataSource.fetchRecipes(any()))
+            .thenThrow(exception)
+
+        //Act
+        val result = recipesPagingSource.load(
+            PagingSource.LoadParams.Refresh(
+                null,
+                2,
+                false
+            )
+        )
+
+        //Assert
+        assertEquals(
+            PagingSource.LoadResult.Error<Int, RecipeFactory.Recipe>(exception),
             result
         )
     }
