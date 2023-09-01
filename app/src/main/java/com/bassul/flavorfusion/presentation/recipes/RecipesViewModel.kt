d9ce76cd02db23c45fe2f4d0ea907dcebca45ee2
mellinacerqueira-hotmart <mellina.cerqueira@hotmart.com>
2023-08-31 21:12:34 -0300

    Evitando recarregar a lista de receitas ao voltar da tela de detalhes e adicionado animação no voltar

--- app/src/main/java/com/bassul/flavorfusion/presentation/recipes/RecipesViewModel.kt
+++ app/src/main/java/com/bassul/flavorfusion/presentation/recipes/RecipesViewModel.kt
@@ -1,20 +1,43 @@
 package com.bassul.flavorfusion.presentation.recipes
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.distinctUntilChanged
+import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.bassul.core.domain.model.Recipe
 import com.bassul.core.usecase.GetRecipesUseCase
+import com.bassul.core.usecase.base.CoroutinesDispatchers
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class RecipesViewModel @Inject constructor(
-    private val getRecipesUseCase: GetRecipesUseCase
+    private val getRecipesUseCase: GetRecipesUseCase,
+    coroutinesDispatchers: CoroutinesDispatchers
 ) : ViewModel() {
+
+    private val action = MutableLiveData<Action>()
+    val state: LiveData<UiState> = action
+        .distinctUntilChanged()
+        .switchMap { action ->
+            when (action) {
+                is Action.Search -> {
+                    getRecipesUseCase(
+                        GetRecipesUseCase.GetRecipesParams(action.query, getPageConfig())
+                    ).cachedIn(viewModelScope).map {
+                        UiState.SearchResult(it)
+                    }.asLiveData(coroutinesDispatchers.main())
+                }
+            }
+        }
 
     fun recipesPagingData(query: String): Flow<PagingData<Recipe>> {
         return getRecipesUseCase(
@@ -25,4 +48,16 @@ class RecipesViewModel @Inject constructor(
     private fun getPageConfig() = PagingConfig(
         pageSize = 2
     )
+
+    fun searchRecipes(query: String = "") {
+        action.value = Action.Search(query)
+    }
+
+    sealed class UiState {
+        data class SearchResult(val data: PagingData<Recipe>): UiState()
+    }
+
+    sealed class Action {
+        data class Search(val query: String) : Action()
+    }
 }
